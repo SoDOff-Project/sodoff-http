ee4a2734e174eb28c838c46ee1702436ea4c39f6
Spirtix <michal.prazak1@gmail.com>
2025-02-25 18:41:32 +0100

    http post support

--- sodoff-http-native/library.c
+++ sodoff-http-native/library.c
@@ -109,6 +109,112 @@ __declspec(dllexport) char* https_get(const char* url) {
     return buffer;
 }
 
+__declspec(dllexport) char* https_post(const char* url, const char* form) {
+    int wlen = MultiByteToWideChar(CP_UTF8, 0, url, -1, NULL, 0);
+    if (!wlen)
+        return NULL;
+    wchar_t* wurl = malloc(wlen * sizeof(*wurl));
+    if (!wurl)
+        return NULL;
+    MultiByteToWideChar(CP_UTF8, 0, url, -1, wurl, wlen);
+
+    URL_COMPONENTS uc = { sizeof(uc) };
+    wchar_t hostName[256] = { 0 };
+    wchar_t urlPath[1024] = { 0 };
+    uc.lpszHostName = hostName;
+    uc.dwHostNameLength = 256;
+    uc.lpszUrlPath = urlPath;
+    uc.dwUrlPathLength = 1024;
+    if (!WinHttpCrackUrl(wurl, 0, 0, &uc)) {
+        free(wurl);
+        return NULL;
+    }
+
+    HINTERNET hSession = WinHttpOpen(L"sodoff-https/1.0",
+        WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+        WINHTTP_NO_PROXY_NAME,
+        WINHTTP_NO_PROXY_BYPASS,
+        0);
+    if (!hSession) {
+        free(wurl);
+        return NULL;
+    }
+    DWORD secureProtocols = WINHTTP_FLAG_SECURE_PROTOCOL_TLS1_2;
+    WinHttpSetOption(hSession, WINHTTP_OPTION_SECURE_PROTOCOLS, &secureProtocols, sizeof(secureProtocols));
+
+    HINTERNET hConnect = WinHttpConnect(hSession, hostName, uc.nPort, 0);
+    if (!hConnect) {
+        WinHttpCloseHandle(hSession);
+        free(wurl);
+        return NULL;
+    }
+
+    HINTERNET hRequest = WinHttpOpenRequest(hConnect,
+        L"POST",
+        urlPath,
+        NULL,
+        WINHTTP_NO_REFERER,
+        WINHTTP_DEFAULT_ACCEPT_TYPES,
+        WINHTTP_FLAG_SECURE);
+    if (!hRequest) {
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        free(wurl);
+        return NULL;
+    }
+
+    wchar_t headers[] = L"Content-Type: application/x-www-form-urlencoded\r\n";
+    DWORD formLength = form ? (DWORD)strlen(form) : 0;
+
+    BOOL bResults = WinHttpSendRequest(hRequest,
+        headers,
+        -1,
+        (LPVOID)form,
+        formLength,
+        formLength,
+        0);
+    if (!bResults) {
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        free(wurl);
+        return NULL;
+    }
+
+    bResults = WinHttpReceiveResponse(hRequest, NULL);
+    if (!bResults) {
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hConnect);
+        WinHttpCloseHandle(hSession);
+        free(wurl);
+        return NULL;
+    }
+
+    char* buffer = NULL;
+    DWORD totalSize = 0, size = 0;
+    while (WinHttpQueryDataAvailable(hRequest, &size) && size) {
+        char* newBuffer = realloc(buffer, totalSize + size + 1);
+        if (!newBuffer) {
+            free(buffer);
+            buffer = NULL;
+            break;
+        }
+        buffer = newBuffer;
+        DWORD read = 0;
+        if (!WinHttpReadData(hRequest, buffer + totalSize, size, &read))
+            break;
+        totalSize += read;
+    }
+    if (buffer)
+        buffer[totalSize] = '\0';
+
+    WinHttpCloseHandle(hRequest);
+    WinHttpCloseHandle(hConnect);
+    WinHttpCloseHandle(hSession);
+    free(wurl);
+    return buffer;
+}
+
 __declspec(dllexport) void free_string(char* str) {
     if (str)
         free(str);
--- sodoff-http-wrapper/SodoffHttpsClient.cs
+++ sodoff-http-wrapper/SodoffHttpsClient.cs
@@ -6,11 +6,24 @@ namespace sodoff_http_wrapper {
         [DllImport("sodoff_http.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
         private static extern IntPtr https_get(string url);
 
+        [DllImport("sodoff_http.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr https_post(string url, string form);
+
         [DllImport("sodoff_http.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern void free_string(IntPtr ptr);
 
         public static string Get(string url) {
             IntPtr ptr = https_get(url);
+            if (ptr == IntPtr.Zero)
+                return "";
+
+            string result = Marshal.PtrToStringAnsi(ptr);
+            free_string(ptr);
+            return result;
+        }
+
+        public static string Post(string url, string form) {
+            IntPtr ptr = https_post(url, form);
             if (ptr == IntPtr.Zero)
                 return "";
 
